8009c769d504896a6aaa0e53fdc01d094d59ebaf
Francesco Lässig <flaessig@student.ethz.ch>
2022-02-25 15:02:20 +0100

    added sliders, max_iterations input, documentation

--- sketch.js
+++ sketch.js
@@ -1,12 +1,63 @@
 
+let max_iterations = 500;
+let color_sliders = [];
+let slider_spacing = 30;
+let color_square_width = 20;
+let colors = ['red', 'green', 'blue', 'alpha'];
+let max_it_input;
 
 function setup() {
     createCanvas(500, 500); 
+    background(0, 0, 0);
+
+    // create color sliders
+    for (var i = 0; i < colors.length; i++) {
+
+        // slider
+        var color_slider = createSlider(0, 255, 100);
+        color_slider.position(width + 50, 10 + i * slider_spacing)
+        color_slider.style('width', '80px');
+        color_sliders.push(color_slider);
+
+        // label
+        var p = createP(colors[i]);
+        p.style('font-size', '16px');
+        p.position(width + color_slider.width + 70, i * slider_spacing);
+    }
+
+    // max iterations input
+    max_it_input = createInput('500');
+    max_it_input.position(width + 50, slider_spacing * 7);
+    var p = createP('max iterations')
+    p.position(width + 50, slider_spacing * 5.5)
+
+    mouseReleased()
 }
   
 
 function draw() {
-    buddhabrot_iteration(this.arr, 500, 1, 0);
+    buddhabrot_iteration(max_iterations, 1);
 }
 
 
+function mouseReleased() {
+    new_color = color(color_sliders[0].value(),
+                      color_sliders[1].value(),
+                      color_sliders[2].value(),
+                      color_sliders[3].value())
+    stroke(new_color);
+
+    fill(new_color);
+    draw_color_square(new_color)
+}
+
+function keyReleased() {
+    max_iterations = parseInt(max_it_input.value())
+}
+
+function draw_color_square(color) {
+    fill(0, 0, 0);
+    rect(width - color_square_width, 0, color_square_width, color_square_width);
+    fill(color);
+    rect(width - color_square_width, 0, color_square_width, color_square_width);
+}
--- src/buddhabrot.js
+++ src/buddhabrot.js
@@ -23,20 +23,16 @@ function num_iterations(c, max_iterations) {
     return parseFloat(n)
 }
 
-function trace_escape(arr, c, max_iterations, zoom, color_num) {
+function trace_escape(c, max_iterations, zoom) {
     var z = Complex(0, 0)
     for (var n = 0; n < max_iterations; n++) {
         if (z.abs() > 2) {break}
         z = this.iteration(z, c)
         var pixels = this.to_pixels(z.re, z.im, zoom)
         if (pixels.u >= 1 && pixels.u < width - 1 && pixels.v >= 1 && pixels.v < height - 1) {
-            //arr[pixels.u][pixels.v][color_num] += 1
-            //arr[pixels.u][pixels.v] += 1
-            stroke(color(0, 0, 0, 50))
             point(pixels.u, pixels.v)
         }
     }
-    return arr
 }
 
 function draw_mandelbrot(max_iterations, zoom) {
@@ -53,27 +49,43 @@ function draw_mandelbrot(max_iterations, zoom) {
     }
 }
 
-function buddhabrot_iteration(arr, max_iterations, zoom, color_num) {
+function buddhabrot_iteration(max_iterations, zoom) {
+
+    /**
+     * Computes mandelbrot trajectories of random complex numbers until it finds one that is certain to escape
+     * within `max_iterations` iterations. The escaping trajectory is drawn upon the canvas.
+     * 
+     * @param  {Integer} max_iterations  The maximal number of iterations of the Mandelbrot function tried
+     *                                   to see if a random trajectory escapes. If it escapes before
+     *                                   max_iterations, then it is kept and drawn onto the canvas.
+     *                                   The higher the value, the bigger the impact of trajectories that spend
+     *                                   a long time in a 'confined space'.
+     * @param  {Float} zoom              How much we zoom into buddhabrot. TODO: needs to be tested.
+     */
 
     var iterations = max_iterations - 1
     while (iterations == max_iterations - 1) {
+
+        // choose a random starting point for the mandelbrot iterations
         var x = Math.random() * 2 - 1.5
         var y = Math.random() * 2 - 1
-
         var c = Complex(x, y)
-        var t = c.arg()
-        var cardiod_point = Complex((2 * Math.cos(t) - Math.cos(2*t)) / 4, (2 * Math.sin(t) - Math.sin(2*t)) / 4)
 
         // skip points within main cardiod and within circle with center (-1, 0)
+        var t = c.arg()
+        var cardiod_point = Complex((2 * Math.cos(t) - Math.cos(2*t)) / 4, (2 * Math.sin(t) - Math.sin(2*t)) / 4)
         if (!(c.abs() > cardiod_point.abs() && c.add(1).abs() > 0.25)) {continue}
         
+        // compute the number of iterations, up to `max_iterations`, that could be performed
+        // without being certain that the trajectory escapes
         var iterations = num_iterations(c, max_iterations)
         
+        // fewer than `max_iterations` steps could be performed, then the trajectory escaped and is
+        // part of buddhabrot
         if (iterations < max_iterations - 1) {
-            arr = this.trace_escape(arr, c, max_iterations, zoom, color_num)
+            this.trace_escape(c, max_iterations, zoom)
         }
 
-        //return arr, parseInt(iterations)
     }
 }
 
